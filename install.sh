70dc644f400ebde7c3fb2a887a849e7533e39601
DeFliTeam <130003530+DeFliTeam@users.noreply.github.com>
2023-11-25 21:10:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-REPO="https://github.com/defliteam/client-feeder.git"
+REPO="https://github.com/defliteam/client-feeder/main"
 BRANCH="master"
-IPATH="https://github.com/defliteam/client-feeder"
+IPATH="https://github.com/defliteam/client-feeder/main"
 mkdir -p $IPATH
 
 if [ "$(id -u)" != "0" ]; then
