fdfba0c3252c400c21bfc3cdc66d24df7c4df036
DeFliTeam <130003530+DeFliTeam@users.noreply.github.com>
2023-11-25 19:59:21 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -47,7 +47,7 @@ BACKTITLETEXT="DeFli Networks Setup Script"
 
 whiptail --backtitle "$BACKTITLETEXT" --title "$BACKTITLETEXT" --yesno "Thanks for choosing to share your data with defli!\n\ndelfi network uses your data to support infrastructure for unmanned aviation. This script will configure your current ADS-B receiver to share your feeders data with DeFli.\n\nWould you like to continue setup?" 13 78 || abort
 
-defliUSERNAME=$(whiptail --backtitle "$BACKTITLETEXT" --title "Feeder MLAT Name" --nocancel --inputbox "\nPlease enter a unique name to be shown on the MLAT map (the pin will be offset for privacy)\n\nExample: \"william34-london\", \"william34-jersey\", etc.\nDisable MLAT: enter a zero: 0" 12 78 3>&1 1>&2 2>&3) || abort
+defliUSERNAME=$(whiptail --backtitle "$BACKTITLETEXT" --title "Feeder MLAT Name" --nocancel --inputbox "\nPlease enter your unique name to be shown on the MLAT map (the pin will be offset for privacy)\n\nExample: \"william34-london\", \"william34-jersey\", etc.\nDisable MLAT: enter a zero: 0" 12 78 3>&1 1>&2 2>&3) || abort
 
 NOSPACENAME="$(echo -n -e "${defliUSERNAME}" | tr -c '[a-zA-Z0-9]_\- ' '_')"
 
