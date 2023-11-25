9e31576adeb2b560d468d87e70b6f0d81bcfa0a0
DeFliTeam <130003530+DeFliTeam@users.noreply.github.com>
2023-11-25 19:06:29 +0000

    Create defli-mlat.sh

---
+++ scripts/defli-mlat.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if grep -qs -e 'LATITUDE' /boot/defli-config.txt &>/dev/null && [[ -f /boot/defli-env ]]; then
+    source /boot/defli-config.txt
+    source /boot/defli-env
+else
+    source /etc/default/defli
+fi
+
+if [[ "$LATITUDE" == 0 ]] || [[ "$LONGITUDE" == 0 ]] || [[ "$USER" == 0 ]] || [[ "$USER" == "disable" ]]; then
+    echo MLAT DISABLED
+    sleep 3600
+    exit
+fi
+
+INPUT_IP=$(echo $INPUT | cut -d: -f1)
+INPUT_PORT=$(echo $INPUT | cut -d: -f2)
+
+sleep 2
+
+while ! nc -z "$INPUT_IP" "$INPUT_PORT" && command -v nc &>/dev/null; do
+    echo "Could not connect to $INPUT_IP:$INPUT_PORT, retry in 10 seconds."
+    sleep 10
+done
+
+exec /usr/local/share/defli/venv/bin/mlat-client \
+    --input-type "$INPUT_TYPE" --no-udp \
+    --input-connect "$INPUT" \
+    --server "$MLATSERVER" \
+    --user "$USER" \
+    --lat "$LATITUDE" \
+    --lon "$LONGITUDE" \
+    --alt "$ALTITUDE" \
+    $PRIVACY \
+    --uuid-file /usr/local/share/defliteam/defli-uuid \
+    $RESULTS $RESULTS1 $RESULTS2 $RESULTS3 $RESULTS4
